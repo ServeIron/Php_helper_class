bfe020089ac79449241403c7a9cae4dc2527fc52
ServeIron <servetgkdmr@gmail.com>
2020-07-27 03:06:50 +0300

    sifreleme onarılıyor

--- Yardimci_Sinif.php
+++ Yardimci_Sinif.php
@@ -20,6 +20,45 @@
 
         return $this->turkce_cevir($sonuc);
       }
+
+      //Xssl açığı kapatma, form güvenliği 
+      function html_temizle($veri){
+          
+        if($veri == ' " ' && $veri == " ' " || empty($veri) || $veri == " "){
+
+            echo "hatalı";
+        }
+          
+        else{
+      
+            $form_kontrol = strip_tags(htmlentities(htmlspecialchars($veri)));
+
+            $xssl_karakterler = array(
+                '?','&','gt',';','&quot','&lt','&gt','amp','"',
+            );
+            $temizle = array(
+                '', '', '', '', '', '','','','',
+            );
+            return str_replace($xssl_karakterler,$temizle, $form_kontrol);
+          
+        }      
+    
+      }
+/* 
+Şifre oluştur düzenlenecek.
+*/
+      function sifre_olustur($sifre){ 
+        $encrypt_method = 'AES-256-CBC'; //sifreleme yontemi
+        $secret_key = '11*_33'; //sifreleme anahtari
+        $secret_iv = '22-=**_'; //gerekli sifreleme baslama vektoru
+        $key = hash('sha256', $secret_key); //anahtar hast fonksiyonu ile sha256 algoritmasi ile sifreleniyor
+        $iv = substr(hash('sha256', $secret_iv), 0, 16); 
+        
+        $sifrelendi = openssl_encrypt($sifre,$encrypt_method, $key, false, $iv);        
+        
+        $sifre_cozuldu = openssl_decrypt($sifrelendi,$encrypt_method, $key, false, $iv);
+        
+      }
   }
 
 ?>
--- index.php
+++ index.php
@@ -4,8 +4,17 @@ include "Yardimci_Sinif.php";
 
 $cek = new Yardimci_Sinif();
 
+$string = 'denemeYazi';
+
+$encrypt_method = 'AES-256-CBC'; //sifreleme yontemi
+$secret_key = '11*_33'; //sifreleme anahtari
+$secret_iv = '22-=**_'; //gerekli sifreleme baslama vektoru
+$key = hash('sha256', $secret_key); //anahtar hast fonksiyonu ile sha256 algoritmasi ile sifreleniyor
+$iv = substr(hash('sha256', $secret_iv), 0, 16); 
+
+$sifrelendi = openssl_encrypt($string,$encrypt_method, $key, false, $iv);
 
 
-$metin = "Bu bir deneme yazısı ğşl";
-echo $cek->seo_url_olustur($metin);
+$sifre_cozuldu = openssl_decrypt($sifrelendi,$encrypt_method, $key, false, $iv);
+
 ?>
