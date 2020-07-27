dd0459277cd82f8dcfac177f57dfbefd89fdec1d
ServeIron <servetgkdmr@gmail.com>
2020-07-27 20:03:44 +0300

    sifreleme tamam

--- Yardimci_Sinif.php
+++ Yardimci_Sinif.php
@@ -21,44 +21,60 @@
         return $this->turkce_cevir($sonuc);
       }
 
-      //Xssl açığı kapatma, form güvenliği 
-      function html_temizle($veri){
-          
-        if($veri == ' " ' && $veri == " ' " || empty($veri) || $veri == " "){
+   
+      function html_temizle(){
 
-            echo "hatalı";
+        $gelen_arg_sayisi = func_num_args();
+        
+        for($i =0; $i < $gelen_arg_sayisi; $i++){
+                                 
+              $veri = func_get_arg($i);
+
+              $form_kontrol = strip_tags(htmlentities(htmlspecialchars($veri)));
+
+              $xssl_karakterler = array(
+                  '?','&','gt',';','&quot','&lt','&gt','amp','"',
+              );
+              $temizle = array(
+                  '', '', '', '', '', '','','','',
+              );
+
+            $sonucla[] = str_replace($xssl_karakterler,$temizle, $form_kontrol);          
+                       
+         
         }
-          
-        else{
-      
-            $form_kontrol = strip_tags(htmlentities(htmlspecialchars($veri)));
-
-            $xssl_karakterler = array(
-                '?','&','gt',';','&quot','&lt','&gt','amp','"',
-            );
-            $temizle = array(
-                '', '', '', '', '', '','','','',
-            );
-            return str_replace($xssl_karakterler,$temizle, $form_kontrol);
-          
-        }      
+  
+        print_r($sonucla);
     
       }
-/* 
-Şifre oluştur düzenlenecek.
-*/
-      function sifre_olustur($sifre){ 
+
+   // true şifreli verir false ise çözer   
+      function sifre_olustur($sifre,$kontrol = true){ 
+
         $encrypt_method = 'AES-256-CBC'; //sifreleme yontemi
+
         $secret_key = '11*_33'; //sifreleme anahtari
+
         $secret_iv = '22-=**_'; //gerekli sifreleme baslama vektoru
+
         $key = hash('sha256', $secret_key); //anahtar hast fonksiyonu ile sha256 algoritmasi ile sifreleniyor
-        $iv = substr(hash('sha256', $secret_iv), 0, 16); 
         
-        $sifrelendi = openssl_encrypt($sifre,$encrypt_method, $key, false, $iv);        
-        
+        $iv = substr(hash('sha256', $secret_iv), 0, 16);     
+        $sifrelendi = openssl_encrypt($sifre,$encrypt_method, $key, false, $iv);  
+
         $sifre_cozuldu = openssl_decrypt($sifrelendi,$encrypt_method, $key, false, $iv);
-        
+
+          if($kontrol == true){          
+            return $sifrelendi;
+          }
+          else if($kontrol == false){
+
+            return $sifre_cozuldu;
+
+          }
+                
       }
+
+ 
   }
 
-?>
--- index.php
+++ index.php
@@ -1,20 +1,7 @@
 <?php 
-
 include "Yardimci_Sinif.php";
 
 $cek = new Yardimci_Sinif();
 
-$string = 'denemeYazi';
-
-$encrypt_method = 'AES-256-CBC'; //sifreleme yontemi
-$secret_key = '11*_33'; //sifreleme anahtari
-$secret_iv = '22-=**_'; //gerekli sifreleme baslama vektoru
-$key = hash('sha256', $secret_key); //anahtar hast fonksiyonu ile sha256 algoritmasi ile sifreleniyor
-$iv = substr(hash('sha256', $secret_iv), 0, 16); 
-
-$sifrelendi = openssl_encrypt($string,$encrypt_method, $key, false, $iv);
-
-
-$sifre_cozuldu = openssl_decrypt($sifrelendi,$encrypt_method, $key, false, $iv);
 
 ?>
