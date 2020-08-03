13524cffcceb6b2b2d93acd8bf8c21a4d49ea4af
ServeIron <servetgkdmr@gmail.com>
2020-08-03 22:01:36 +0300

    çoklu dosya yükleme tamamlandı

--- Yardimci_Sinif.php
+++ Yardimci_Sinif.php
@@ -14,6 +14,7 @@
       }
 
       function seo_url_olustur($cumle){
+
         $dizi =  explode(" ", $cumle);
         $ayrac = implode("-",$dizi);
         $sonuc = mb_strtolower($ayrac,"UTF-8");
@@ -22,7 +23,7 @@
       }
 
    
-      function html_temizle(){
+      function xss_temizle(){
 
         $gelen_arg_sayisi = func_num_args();
         
@@ -44,7 +45,7 @@
          
         }
   
-        print_r($sonucla);
+       return $sonucla;
     
       }
 
@@ -64,7 +65,8 @@
 
         $sifre_cozuldu = openssl_decrypt($sifrelendi,$encrypt_method, $key, false, $iv);
 
-          if($kontrol == true){          
+          if($kontrol == true){      
+
             return $sifrelendi;
           }
           else if($kontrol == false){
@@ -74,6 +76,109 @@
           }
                 
       }
+
+
+      function tc_id($get_tc){
+
+        $arr = str_split($get_tc);
+    
+        if(count($arr) == 11 && is_int($get_tc)){
+    
+            $toplam = array_sum($arr);
+    
+            $on = ((($arr[0] + $arr[2] + $arr[4] + $arr[6] + $arr[8]) * 7) - ($arr[1] + $arr[3] + $arr[5] + $arr[7])) % 10; 
+    
+            $onbir = ($toplam - $arr[10]) % 10;
+    
+            echo $on == $arr[9] && $onbir == $arr[10] ? "Tc kimlik numarası doğru." : "Tc kimlik numarası yanlış";
+    
+           
+        }
+        else{
+    
+            echo "Girdiğiniz değer 11 haneli ve rakam olmalı.";
+            
+         }
+      
+    }
+
+
+
+function multi_file_up($file_nme,$file_type,$file_size,$file_base){
+
+            if($_POST["submit"])
+            {
+                $filecount = count($_FILES[$file_nme]['name']);
+
+                $file_type_select = $file_type;
+
+                $uyari = array();
+                $kont = array();
+
+                for($i = 0; $i < $filecount; $i++){
+                                
+                    $fileName = substr($_FILES[$file_nme]['name'][$i],-4,4);
+
+                    $fileRandName= rand(0,999999).$fileName;
+
+                    $fileSize = $_FILES[$file_nme]['size'][$i];
+
+                    $fileTmp = $_FILES[$file_nme]['tmp_name'][$i];
+
+                    $fileType = explode('.',$_FILES[$file_nme]['type'][$i]);
+
+                        foreach($fileType as $cek){
+
+                            if(in_array($cek,$file_type_select) === false){
+
+                                $kont[] = "false";
+
+                                $uyari[] = "Dosya tipi uyuşmuyor";   
+                            
+                            break;
+
+                            }
+
+                            if($fileSize > $file_size) {
+
+                                $kont[] = "false";
+
+                                $uyari[] = "Dosya boyutu fazla.";
+
+                            break;
+                        
+                            }
+
+                            $deneme[] = $fileRandName;
+
+                            $file_tp[] = $fileTmp;
+                                    
+                        }
+
+                    
+                } 
+                        if(in_array("false",$kont)){
+                        
+                            return $uyari;
+                        
+                        }
+                        else 
+                        {
+                              for($k = 0; $k < count($deneme); $k++)
+                              {
+
+                                     move_uploaded_file($file_tp[$k],$file_base.$deneme[$k]);
+                                     
+                              }
+                               
+                              return $deneme;
+                        }
+
+            }
+       }
+
+
+
 
  
   }
--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php 
 include "Yardimci_Sinif.php";
-
+error_reporting(0);
 $cek = new Yardimci_Sinif();
 
 
- $cek->html_temizle(1,2,"<?php 'alt ?>");
+
+
 ?>
+
