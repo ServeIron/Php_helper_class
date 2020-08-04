a9b930e3170f0907f2ee8660d84eaa6a02a7bb09
ServeIron <servetgkdmr@gmail.com>
2020-08-04 13:54:38 +0300

    ..

--- index.php
+++ index.php
@@ -4,11 +4,7 @@ error_reporting(0);
 $cek = new Yardimci_Sinif();
 
   
-$gel = $cek->multi_file_up("name",array("image/png"),50000,'dosya/');
-print_r($gel);
+
 ?>
 
-<form action="" method="POST" enctype="multipart/form-data">
-         <input type="file" name="name[]" id="name" multiple />
-         <input type="submit" name="submit"/>
-      </form>
+
